844241513ce8430c4781e54456233148a12551d5
Anton Nikolenko <anton.nikolenko@axiom.bet>
2024-03-18 23:44:01 +0200

    chore: temp commit

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import TelegramBot from 'node-telegram-bot-api';
+import { config } from 'dotenv';
+config();
+// Замените 'YOUR_TELEGRAM_BOT_TOKEN' на токен вашего бота
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+const channelUsername = process.env.CHANNEL_NAME;
+const channelLink = `https://t.me/${channelUsername}`;
+
+bot.on('channel_post', async (msg) => {
+  try {
+    const chatId = msg.chat.id;
+    const messageId = msg.message_id;
+    const caption = msg.caption || '';
+    const signature = `\n\n\n[Подписывайтесь на канал](${channelLink})`;
+
+    // Удаляем оригинальное сообщение
+    await bot.deleteMessage(chatId, messageId.toString());
+
+    // Проверяем наличие фото
+    if (msg.photo) {
+      const photo = msg.photo[msg.photo.length - 1].file_id; // Берем последнее фото (самое качественное)
+      await bot.sendPhoto(chatId, photo, {
+        caption: caption + signature,
+        parse_mode: 'Markdown',
+      });
+    }
+    // Проверяем наличие видео
+    else if (msg.video) {
+      const video = msg.video.file_id;
+      await bot.sendVideo(chatId, video, {
+        caption: caption + signature,
+        parse_mode: 'Markdown',
+      });
+    }
+    // Обработка текстовых сообщений
+    else if (msg.text) {
+      await bot.sendMessage(chatId, msg.text + signature, {
+        parse_mode: 'Markdown',
+      });
+    }
+  } catch (error) {
+    console.error('Произошла ошибка при обработке сообщения:', error);
+  }
+});
+
+console.log('Бот запущен...');
