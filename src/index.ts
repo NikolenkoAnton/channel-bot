a15a9b01e1fa787956b5e1453d211e084a35a8bf
Anton Nikolenko <anton.nikolenko@axiom.bet>
2024-03-19 10:12:14 +0200

    chore: temp commit

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,41 @@ import TelegramBot from 'node-telegram-bot-api';
 import { config } from 'dotenv';
 config();
 // Замените 'YOUR_TELEGRAM_BOT_TOKEN' на токен вашего бота
-const token =
-  process.env.TELEGRAM_BOT_TOKEN ||
-  '7142305379:AAGtqs1yBJGfmjStgdR0piqBaXsm0woC09Y';
+const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
-const channelUsername = process.env.CHANNEL_NAME || 'bannedwar';
+const channelUsername = process.env.CHANNEL_NAME;
 const channelLink = `https://t.me/${channelUsername}`;
+
+const mediaGroups: {
+  [key: string]: TelegramBot.InputMediaPhoto[] | TelegramBot.InputMediaVideo[];
+} = {};
 
 bot.on('channel_post', async (msg) => {
   try {
     const chatId = msg.chat.id;
     const messageId = msg.message_id;
     const caption = msg.caption || '';
+    if (msg.forward_from_message_id) {
+      return;
+    }
     const signature = `\n\n\n[Подписывайтесь на канал](${channelLink})`;
 
+    if (!caption && msg.text) {
+      await bot.editMessageText(msg.text || '' + signature, {
+        chat_id: chatId,
+        message_id: messageId,
+        parse_mode: 'Markdown',
+      });
+    } else {
+      await bot.editMessageCaption(caption || '' + signature, {
+        chat_id: chatId,
+        message_id: messageId,
+        parse_mode: 'Markdown',
+      });
+    }
     // Удаляем оригинальное сообщение
-    await bot.deleteMessage(chatId, messageId.toString());
-
+    // await bot.deleteMessage(chatId, messageId.toString());
+    /* 
     // Проверяем наличие фото
     if (msg.photo) {
       const photo = msg.photo[msg.photo.length - 1].file_id; // Берем последнее фото (самое качественное)
@@ -40,7 +58,7 @@ bot.on('channel_post', async (msg) => {
       await bot.sendMessage(chatId, msg.text + signature, {
         parse_mode: 'Markdown',
       });
-    }
+    } */
   } catch (error) {
     console.error('Произошла ошибка при обработке сообщения:', error);
   }
